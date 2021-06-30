b2eccd167692c5d4cc8fc48fc4363f4fa3d31c78
hayzee <hayzee@users.noreply.github.com>
2021-06-30 11:18:40 +0100

    More answers

--- src/solutions4clojure/q029_get_the_caps.clj
+++ src/solutions4clojure/q029_get_the_caps.clj
@@ -1,3 +1,8 @@
 (ns solutions4clojure.q029-get-the-caps)
 
-(def __ (fn [x] (apply str (filter #(<= 65 (int %) 90) x))))
+(set! *warn-on-reflection* true)
+
+(def __ (fn [s] (apply str (filter #(Character/isUpperCase ^char %) s))))
+
+;; This is OK, but it relies on ascii encoding.
+;; (def __ (fn [x] (apply str (filter #(<= 65 (int %) 90) x))))
