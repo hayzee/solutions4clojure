a3d77abca81dc24a3c794e4c4373aca2aafdebc7
hayzee <hayzee@users.noreply.github.com>
2021-07-05 22:12:17 +0100

    More answers

---
+++ src/solutions4clojure/q131_sum_some_set_subsets.clj
@@ -0,0 +1,19 @@
+(ns solutions4clojure.q131-sum-some-set-subsets)
+
+(def __
+  (fn [& sets]
+    (let
+      [subseqs (fn subseqs [s]
+                 (if (empty? s)
+                   '(())
+                   (concat (subseqs (rest s)) (map (partial cons (first s)) (subseqs (rest s))))))
+       subsets (fn [s]
+                 (filter (complement empty?) (set (map set (subseqs (set s))))))
+       set-sumof-subsets
+       (fn [s]
+         (set (map #(apply + %) (subsets s))))
+       ]
+      (->>
+        (map set-sumof-subsets sets)
+        (apply clojure.set/intersection)
+        ((complement empty?))))))
