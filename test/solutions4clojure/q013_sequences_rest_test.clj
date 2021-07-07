9dd85e9c5430149c6f5cfb556608ddcd3b080b25
hayzee <hayzee@users.noreply.github.com>
2021-07-07 20:11:58 +0100

    More answers

--- test/solutions4clojure/q013_sequences_rest_test.clj
+++ test/solutions4clojure/q013_sequences_rest_test.clj
@@ -2,5 +2,5 @@
   (:require [clojure.test :refer :all])
   (:require [solutions4clojure.q013-sequences-rest :refer :all]))
 
-(deftest check_answer
+(deftest answer-test
   (is (= __ (rest [10 20 30 40]))))
