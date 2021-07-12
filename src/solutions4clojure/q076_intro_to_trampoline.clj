6991f39fe5b677484b54d9a9e1f7511be42c6397
hayzee <hayzee@users.noreply.github.com>
2021-07-12 22:27:21 +0100

    More answers

---
+++ src/solutions4clojure/q076_intro_to_trampoline.clj
@@ -0,0 +1,4 @@
+(ns solutions4clojure.q076-intro-to-trampoline)
+
+(def __
+  [1 3 5 7 9 11])
---
+++ test/solutions4clojure/q076_intro_to_trampoline_test.clj
@@ -0,0 +1,12 @@
+(ns solutions4clojure.q076-intro-to-trampoline-test
+  (:require [clojure.test :refer :all])
+  (:require [solutions4clojure.q076-intro-to-trampoline :refer :all]))
+
+(deftest answer-test
+  (is (= __
+        (letfn
+          [(foo [x y] #(bar (conj x y) y))
+           (bar [x y] (if (> (last x) 10)
+                        x
+                        #(foo x (+ 2 y))))]
+          (trampoline foo [] 1)))))
