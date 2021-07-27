ce9732953fa871ca5036aa9da524dd5eaad95348
hayzee <hayzee@users.noreply.github.com>
2021-07-27 23:21:21 +0100

    Fixed namespace declarations in tests.

---
+++ src/solutions4clojure/q036_let_it_be.clj
@@ -0,0 +1,5 @@
+(ns solutions4clojure.q036-let-it-be
+  (:require [solutions4clojure.answers :refer :all]))
+
+(defanswer __ [x 7 y 3 z 1])
+
---
+++ test/solutions4clojure/q036_let_it_be_test.clj
@@ -0,0 +1,9 @@
+(ns solutions4clojure.q036-let-it-be-test
+  (:require [clojure.test :refer :all])
+  (:require [solutions4clojure.q036-let-it-be :refer :all])
+  (:require [solutions4clojure.answers :as answer]))
+
+(deftest answer-test
+  (answer/is __ (= 10 (let __ (+ x y))))
+  (answer/is __ (= 4 (let __ (+ y z))))
+  (answer/is __ (= 1 (let __ z))))
