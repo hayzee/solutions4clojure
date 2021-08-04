248e14dd171d89e643e7720808b6bd240f1815ed
hayzee <hayzee@users.noreply.github.com>
2021-08-04 08:14:06 +0100

    More answers.

---
+++ src/solutions4clojure/q045_intro_to_iterate.clj
@@ -0,0 +1,3 @@
+(ns solutions4clojure.q045-intro-to-iterate)
+
+(def __ '(1 4 7 10 13))
---
+++ test/solutions4clojure/q045_intro_to_iterate_test.clj
@@ -0,0 +1,6 @@
+(ns solutions4clojure.q045-intro-to-iterate-test
+  (:require [clojure.test :refer :all])
+  (:require [solutions4clojure.q045-intro-to-iterate :refer :all]))
+
+(deftest answer-test
+  (is (= __ (take 5 (iterate #(+ 3 %) 1)))))
