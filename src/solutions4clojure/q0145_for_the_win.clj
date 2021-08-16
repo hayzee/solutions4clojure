4623128cbaa11c3a8145512c9b447358f47039c2
hayzee <hayzee@users.noreply.github.com>
2021-08-16 09:06:23 +0100

    More answers.

---
+++ src/solutions4clojure/q0145_for_the_win.clj
@@ -0,0 +1,9 @@
+(ns solutions4clojure.q0145-for-the-win)
+
+; Clojure's for macro is a tremendously versatile mechanism for producing a sequence based on some other sequence(s).
+; It can take some time to understand how to use it properly, but that investment will be paid back with clear, concise
+; sequence-wrangling later. With that in mind, read over these for expressions (see tests) and try to see how each of
+; them produces the same result.
+
+(def __
+  [1 5 9 13 17 21 25 29 33 37])
---
+++ test/solutions4clojure/q0145_for_the_win_test.clj
@@ -0,0 +1,14 @@
+(ns solutions4clojure.q0145-for-the-win-test
+  (:require [clojure.test :refer :all])
+  (:require [solutions4clojure.q0145-for-the-win :refer :all]))
+
+(deftest answer-test
+  (is (= __ (for [x (range 40)
+                  :when (= 1 (rem x 4))]
+              x)))
+  (is (= __ (for [x (iterate #(+ 4 %) 0)
+                  :let [z (inc x)]
+                  :while (< z 40)]
+              z)))
+  (is (= __ (for [[x y] (partition 2 (range 20))]
+              (+ x y)))))
