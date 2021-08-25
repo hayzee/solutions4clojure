4319e2f31b66c2001eaaebaffbb9e6ded5dee66e
hayzee <hayzee@users.noreply.github.com>
2021-08-25 18:25:54 +0100

    More answers.

--- src/solutions4clojure/q148_the_big_divide.clj
+++ src/solutions4clojure/q148_the_big_divide.clj
@@ -1,5 +1,9 @@
 (ns solutions4clojure.q148-the-big-divide)
 
+; Write a function which calculates the sum of all natural numbers under n (first argument) which are evenly divisible
+; by at least one of a and b (second and third argument). Numbers a and b are guaranteed to be coprimes. Note: Some test
+; cases have a very large n, so the most obvious solution will exceed the time limit.
+
 (def __
   (fn [n a b]
     (let [f #(quot (dec n) %)
