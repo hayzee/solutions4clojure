6145bb5f2e3f219af498cbf7e57c482884a58200
hayzee <hayzee@users.noreply.github.com>
2021-08-27 12:48:41 +0100

    More answers.

--- src/solutions4clojure/q002_simple_math.clj
+++ src/solutions4clojure/q002_simple_math.clj
@@ -1,3 +1,9 @@
 (ns solutions4clojure.q002-simple-math)
 
+; Difficulty: Elementary
+; Topics:
+
+; If you are not familiar with polish notation, simple arithmetic might seem confusing.
+; Note: Enter only enough to fill in the blank (in this case, a single number) - do not retype the whole problem.
+
 (def __ 4)
