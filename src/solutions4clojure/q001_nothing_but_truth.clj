52ea67aeec19bce0a72c0b1882ccac5c80963030
hayzee <hayzee@users.noreply.github.com>
2021-08-27 12:46:46 +0100

    More answers.

--- src/solutions4clojure/q001_nothing_but_truth.clj
+++ src/solutions4clojure/q001_nothing_but_truth.clj
@@ -1,3 +1,9 @@
 (ns solutions4clojure.q001-nothing-but-truth)
 
+; Difficulty: Elementary
+; Topics:
+
+; This is a clojure form. Enter a value which will make the form evaluate to true. Don't over think it! If you are
+; confused, see the getting started page. Hint: true is equal to true.
+
 (def __ true)
