e07c8f3b731c75684a7f269e8d60c373cc68b201
hayzee <hayzee@users.noreply.github.com>
2021-08-27 11:48:11 +0100

    More answers.

--- src/solutions4clojure/scratch.clj
+++ src/solutions4clojure/scratch.clj
@@ -3,6 +3,7 @@
 ;; Incomplete
 ;; ==========
 ;;
-;; 124: Analyse reversi
-;; 127: Love triangle
-;; 140: Veitch, Please!
+;; 124: Analyse reversi         - https://web.archive.org/web/20200701030558/http://www.4clojure.com/problem/124
+;; 127: Love triangle           - https://web.archive.org/web/20200701030613/http://www.4clojure.com/problem/127
+;; 140: Veitch, Please!         - https://web.archive.org/web/20200701030704/http://www.4clojure.com/problem/140
+;; 164: Language of a DFA       - https://web.archive.org/web/20200701030833/http://www.4clojure.com/problem/164
