12c0451505b9b28a42496c6e7354d4922bf424df
hayzee <hayzee@users.noreply.github.com>
2021-09-05 09:37:24 +0100

    Working on: 124-analyze-reversi

--- src/solutions4clojure/q010_intro_to_maps.clj
+++ src/solutions4clojure/q010_intro_to_maps.clj
@@ -1,3 +1,9 @@
 (ns solutions4clojure.q010-intro-to-maps)
 
+; Difficulty:	Elementary
+; Topics:
+
+; Maps store key-value pairs. Both maps and keywords can be used as lookup functions. Commas can be used to make maps
+; more readable, but they are not required.
+
 (def __ 20)
