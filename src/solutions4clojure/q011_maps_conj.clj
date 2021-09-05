7675791060779027b0c0a12246e330a6a7315d2f
hayzee <hayzee@users.noreply.github.com>
2021-09-05 09:38:30 +0100

    Working on: 124-analyze-reversi

--- src/solutions4clojure/q011_maps_conj.clj
+++ src/solutions4clojure/q011_maps_conj.clj
@@ -1,3 +1,8 @@
 (ns solutions4clojure.q011-maps-conj)
 
+; Difficulty:	Elementary
+; Topics:
+
+; When operating on a map, the conj function returns a new map with one or more key-value pairs "added".
+
 (def __ [:b 2])
