4cff9aff3fdd09204f279c0bf85c23d2547c37c7
hayzee <hayzee@users.noreply.github.com>
2021-09-05 09:33:40 +0100

    Working on: 124-analyze-reversi

--- src/solutions4clojure/q007_vectors_conj.clj
+++ src/solutions4clojure/q007_vectors_conj.clj
@@ -1,3 +1,8 @@
 (ns solutions4clojure.q007-vectors-conj)
 
+; Difficulty:	Elementary
+; Topics:
+
+; When operating on a Vector, the conj function will return a new vector with one or more items "added" to the end.
+
 (def __ '(1 2 3 4))
