5d75116ff0b097b7ee71792a604036c04cacaa95
hayzee <hayzee@users.noreply.github.com>
2021-09-05 09:36:32 +0100

    Working on: 124-analyze-reversi

--- src/solutions4clojure/q009_sets_conj.clj
+++ src/solutions4clojure/q009_sets_conj.clj
@@ -1,3 +1,8 @@
 (ns solutions4clojure.q009-sets-conj)
 
+; Difficulty:	Elementary
+; Topics:
+
+; When operating on a set, the conj function returns a new set with one or more keys "added".
+
 (def __ 2)
